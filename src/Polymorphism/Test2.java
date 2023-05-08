66745ded38fb5a48174caa9905fba8183ddb0ef7
Pradeep-003 <pradeep50953@gmail.com>
2023-05-08 23:18:42 +0530

    Method overriding Rule1

---
+++ src/Polymorphism/Test2.java
@@ -0,0 +1,34 @@
+package Polymorphism;
+
+class A1{
+
+    private void m1(){
+
+        System.out.println("m1-A1");
+
+    }
+
+}
+
+class B1 extends A1{
+
+    void m1(){
+
+        System.out.println("m1-B1");
+
+    }
+
+}
+
+
+public class Test2{
+
+    public static void main(String args[]){
+
+        A1 a=new A1();
+
+       // a.m1(); ( error ) to override super class method with subclass , then superclass method must not be declared as private .
+
+    }
+
+}
