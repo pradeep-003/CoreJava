74ff5b6ffcb58ee1807f7ceb691702c347d9ebec
Pradeep-003 <pradeep50953@gmail.com>
2023-05-15 18:54:39 +0530

    code1.java

---
+++ src/javaCodees/code1.java
@@ -0,0 +1,54 @@
+package javaCodees;
+import java.util.Scanner;
+ class ArraySearch
+{
+     int binarySearch(int[] arr, int low , int mid , int high , int key)
+     {
+         while(low <= high ){
+             if (arr[mid] < key){
+                 low = mid + 1 ;
+             } else if (arr[mid] == key) {
+                 return mid;
+             } else{
+                 high = mid -1 ;
+             }
+             mid = ( low + high )/2 ;
+             if (low > high)
+             {
+                 return -1;
+             }
+         }
+        return -1 ;
+     }
+}
+
+public class code1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scan.nextInt();
+
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter element at index " + i + ": ");
+            numbers[i] = scan.nextInt();
+        }
+
+        int middle =  ((numbers.length)-1)/2 ;
+
+        System.out.println("Enter the value you want to find from array : ");
+
+        int search = scan.nextInt();
+
+        ArraySearch a = new ArraySearch();
+      int s =  a.binarySearch(numbers , 0 , middle , numbers.length-1 , search);
+       if(s == -1){
+           System.out.println("value isn't present");
+       }else {
+           System.out.println("value is present at index : " + s);
+       }
+           }
+}
