a16e01f41672e65db4c2751dfa3bbae4b33edc67
Pradeep-003 <pradeep50953@gmail.com>
2023-07-12 22:44:20 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,1 @@
- @FunctionalInterface
- interface Animal{
-    void safe(String a, String b);
-}
 
-class Main{
-    public static void main(String[] args) {
-       Animal k =( String a, String b )-> System.out.println(a + b);
-       k.safe("pra","deep");
-    }
-}
---
+++ src/javaCodees/GuessingGame.java
@@ -0,0 +1,34 @@
+package javaCodees;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1;
+        int attempts = 0;
+        int guess;
+
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            }
+        } while (guess != secretNumber);
+
+        scanner.close();
+    }
+}
